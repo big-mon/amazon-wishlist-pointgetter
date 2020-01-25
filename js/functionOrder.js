c8ea49ac04451aa694f6a4dfb5173d7a13d9662f
big-mon <mrmon93@gmail.com>
2020-01-25 20:44:44 +0900

    ランキングページでの情報表示に対応

---
+++ js/functionOrder.js
@@ -0,0 +1,9 @@
+// ページ読み込み後の実行処理を制御
+$(document).ready(function() {
+  var url = location.href;
+
+  // [ウィッシュリスト]ページの場合
+  if (url.indexOf("wishlist") != -1) WISHLIST.start();
+  // [ランキング]ページの場合
+  else if (url.indexOf("digital-text") != -1) RANKING.start();
+});
--- js/ranking.js
+++ js/ranking.js
@@ -1,46 +1,25 @@
-var WISHLIST = {
+var RANKING = {
   target: HTMLElement,
-  observer: MutationObserver,
 
-  // アイテム情報の取得と監視の開始
+  // アイテム情報の取得の開始
   start: function() {
     this.setTarget();
     this.searchAndDestroy();
-    this.setObserver();
-    this.startObserv();
   },
 
   // 監視対象とするアイテムリストを設定
   setTarget: function() {
-    WISHLIST.target = document.getElementById("g-items");
-  },
-
-  // DOM変更を検知した場合の処理
-  setObserver: function() {
-    WISHLIST.observer = new MutationObserver(records => {
-      records.forEach(function(record) {
-        // 追加されたノードを対象にループ
-        Array.from(record.addedNodes).forEach(function(node) {
-          // liの場合に処理を実行
-          if (node.nodeName === "LI") WISHLIST.editItem(node);
-        });
-      });
-    });
+    RANKING.target = document.getElementById("zg-ordered-list");
   },
 
   // 対象リストに対してポイント情報を可視化
   searchAndDestroy: function() {
-    var list = WISHLIST.target;
+    var list = RANKING.target;
     $(list)
       .children("li")
       .each(function(index, element) {
-        WISHLIST.editItem(element);
+        RANKING.editItem(element);
       });
-  },
-
-  // DOM変更の監視を開始
-  startObserv: function() {
-    WISHLIST.observer.observe(WISHLIST.target, { childList: true });
   },
 
   // 対象の商品の情報を取得
@@ -50,7 +29,7 @@ var WISHLIST = {
     let url =
       domain +
       $(item)
-        .find("h3.a-size-base .a-link-normal")
+        .find(".zg-item .a-link-normal")
         .attr("href");
 
     // リンク先情報を取得
@@ -68,9 +47,9 @@ var WISHLIST = {
 
         // ポイント情報タグを挿入
         $(item)
-          .find(".price-section")
+          .find(".zg-item")
           .append(
-            '<div class="add-point"><span class="a-text-bold a-color-price">' +
+            '<div class="add-point"><span class="a-size-small a-color-secondary a-color-price">' +
               points +
               "</span></div>"
           );

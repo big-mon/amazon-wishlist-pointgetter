b12f442d95f40fa670ab0483ace64fe67ad9b7c1
BIG-MON <mrmon93@gmail.com>
2020-11-14 12:58:26 +0900

    chore: es6

--- src/js/functionOrder.js
+++ src/js/functionOrder.js
@@ -1,6 +1,6 @@
 // ページ読み込み後の実行処理を制御
 $(document).ready(function() {
-  var url = location.href;
+  let url = location.href;
 
   // [ウィッシュリスト]ページの場合
   if (url.indexOf('wishlist') != -1) WISHLIST.start();

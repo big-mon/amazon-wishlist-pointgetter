f20d59d7f42a9ec2c9fdaf0d21490562cd4a185b
BIG-MON <mrmon93@gmail.com>
2021-07-10 16:01:41 +0900

    refactor: add escape

--- src/js/ranking.js
+++ src/js/ranking.js
@@ -2,58 +2,55 @@ let RANKING = {
   target: HTMLElement,
 
   // アイテム情報の取得の開始
-  start: function() {
+  start: function () {
     this.setTarget();
     this.searchAndDestroy();
   },
 
   // 監視対象とするアイテムリストを設定
-  setTarget: function() {
-    RANKING.target = document.getElementById('zg-ordered-list');
+  setTarget: function () {
+    RANKING.target = document.getElementById("zg-ordered-list");
   },
 
   // 対象リストに対してポイント情報を可視化
-  searchAndDestroy: function() {
+  searchAndDestroy: function () {
     let list = RANKING.target;
     $(list)
-      .children('li')
-      .each(function(index, element) {
+      .children("li")
+      .each(function (index, element) {
         RANKING.editItem(element);
       });
   },
 
   // 対象の商品の情報を取得
-  editItem: function(item) {
+  editItem: function (item) {
     // 商品のURLを取得
-    let domain = location.protocol + '//' + location.host;
-    let url = domain + $(item).find('.zg-item .a-link-normal').attr('href');
+    let domain = location.protocol + "//" + location.host;
+    let url = domain + $(item).find(".zg-item .a-link-normal").attr("href");
 
     // リンク先情報を取得
     fetch(url)
-      .then(res => res.text())
-      .then(data => {
+      .then((res) => res.text())
+      .then((data) => {
         let points = $(data)
           .find(
-            '#addToCart #pointsInsideBuyBox_feature_div span.a-color-price' // Normal Item
-            + ', #addToCart #buyBoxInner span.a-color-price:not(.offer-price)' // Normal Item
-            + ', .loyalty-points .a-align-bottom' // Kindle Item
+            "#addToCart #pointsInsideBuyBox_feature_div span.a-color-price" + // Normal Item
+              ", #addToCart #buyBoxInner span.a-color-price:not(.offer-price)" + // Normal Item
+              ", .loyalty-points .a-align-bottom" // Kindle Item
           )
           .text()
-          .replace(/\t/g, '')
-          .replace(/ /g, '')
-          .replace(/\r?\n/g, '');
-
-          console.log(url);
-          console.log(points);
+          .replace(/\t/g, "")
+          .replace(/ /g, "")
+          .replace(/\r?\n/g, "");
 
         // ポイント情報タグを挿入
         $(item)
-          .find('.zg-item .p13n-sc-price')
+          .find(".zg-item .p13n-sc-price")
           .append(
-            '<span class="add-point a-size-small" style="margin-left: .6rem;"><span class="a-color-price">'
-              + points
-              + '</span></span>'
+            '<span class="add-point a-size-small" style="margin-left: .6rem;"><span class="a-color-price">' +
+              escapeHtml(points) +
+              "</span></span>"
           );
       });
-  }
+  },
 };

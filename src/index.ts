15361fb8bcbc7a898d130e2715bac74aba31e49a
BIGMON <26018409+big-mon@users.noreply.github.com>
2026-08-09 13:54:32 +0900

    refactor: remove redundant wishlist complexity

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
 import { doWishlist } from "./wishlist";
 
-/** 処理全体のスターター */
-const start = () => {
-  let url = location.href;
-
-  // 表示中のページ種別を判定
-  if (url.indexOf("wishlist") != -1) {
-    // [ウィッシュリスト]ページの場合
-    doWishlist();
-  }
-};
-
-start();
+doWishlist();
